a17ae8f96f625f56f999e3be7a24ca7862b02e28
Kwaidan <154322573+Kwaidan@users.noreply.github.com>
2024-01-06 13:13:48 +0300

    Add files via upload

---
+++ module/scroll.js
@@ -0,0 +1,61 @@
+//кнопка прокрутки вверх
+scrollButton();
+function scrollButton() {
+	let btn = createBtn("&#9650;");
+	if(!btn) { return false; }
+	
+	//1. Полная высота документа
+	let scrollHeight = Math.max(
+		document.body.scrollHeight, document.documentElement.scrollHeight,
+		document.body.offsetHeight, document.documentElement.offsetHeight,
+		document.body.clientHeight, document.documentElement.clientHeight
+	);
+	
+	//2. Обработчик события scroll
+	window.addEventListener("scroll", function() {
+		if( window.scrollY > (scrollHeight/2) ) {
+			document.documentElement.append(btn);
+		}else {
+			if(btn) { btn.remove(); }
+		}
+	});
+	
+	//3. Кнопка для прокрутки
+	function createBtn(text) {
+		let btn = document.createElement("button");
+		btn.innerHTML = text;
+		btn.classList.add("scrollButton");
+		document.documentElement.style.scrollBehavior = "smooth";
+		btn.addEventListener("click", function(event) {
+			document.documentElement.scrollTop = 0;
+		});
+		return btn;
+	}
+}
+
+
+//прячем/показываем меню при прокрутке
+scrollMenu();
+function scrollMenu() {
+    let navbar = document.querySelector('.navbar');
+	let sidebar = document.querySelector('.sidebar');
+	let prevScrollpos = window.scrollY;
+	//console.log(prevScrollpos);
+
+    window.addEventListener("scroll", function() {
+        let currentScrollPos = window.scrollY;
+
+        if (prevScrollpos > currentScrollPos) {
+            //document.querySelector(".navbar").style.top = "0";
+            navbar.style.top = "0";
+            sidebar.style.top = "7rem";
+        } 
+        else {
+            //document.querySelector(".navbar").style.top = "-7rem";
+            navbar.style.top = "-7rem";
+            sidebar.style.top = "0";
+        }
+
+        prevScrollpos = currentScrollPos;
+    });
+}
